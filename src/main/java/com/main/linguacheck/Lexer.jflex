4f8821f38386182fce51ba1a813477fc98b7b2e1
KIHM-02 <monteroisaac33@gmail.com>
2025-06-09 17:56:39 -0600

    first commit

---
+++ src/main/java/com/main/linguacheck/Lexer.jflex
@@ -0,0 +1,19 @@
+%%
+
+%class Lexer
+%unicode
+%public
+%type String
+%line
+
+//Definicion de tokens
+WHITESPACE = [ \t\n\r]+
+NUMBER = [0-9]*
+IDENTIFIER = [a-zA-Z][a-zA-Z]*
+
+%%
+
+{WHITESPACE}    {/*Ignoramos los espacios en blanco */}
+{NUMBER}        {return "NUMBER" + yytext(); }
+{IDENTIFIER}    {return "IDENTIFIER"+yytext();}
+. {System.out.println("ERROR: "+yytext());} //Capturamos la palabra mal escrita
---
+++ src/main/java/com/main/linguacheck/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.main.linguacheck;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author monte
+ */
+public class Main {
+
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "HELLO! WELCOME TO LINGUACHECK");
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+}
